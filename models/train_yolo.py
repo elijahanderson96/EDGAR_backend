04109e6f86dbbe73ab94ce15c9305fb44cc69425
Elijah Anderson <eja11001@DESKTOP-83DSK5F.localdomain>
2024-07-11 13:01:47 -0700

    Somehow Extract.py got deleted, readding

--- models/train_yolo.py
+++ models/train_yolo.py
@@ -15,7 +15,7 @@ logging.info("Loading pre-trained YOLOv8 model...")
 model = YOLO('yolov8n.pt')
 
 logging.info("Starting training...")
-model.train(data=os.path.join(output_dir, "data.yaml"), epochs=75)
+model.train(data=os.path.join(output_dir, "data.yaml"), epochs=75, batch=2)
 logging.info("Training complete.")
 
 
@@ -31,8 +31,8 @@ def perform_inference(image_path, model, output_path):
         logging.info(f"Result saved to {result_output_path}")
 
 # Example usage for inference
-perform_inference(
-    r"C:\Users\Elijah\PycharmProjects\edgar_backend\yolo_dataset\images\train\0000320193-19-000010_table_page6_table1.png",
-    model,
-    r"C:\Users\Elijah\PycharmProjects\edgar_backend\test.png"
-)
+# perform_inference(
+#     r"C:\Users\Elijah\PycharmProjects\edgar_backend\yolo_classifier\train\balance_sheet\0001744489-21-000181_table_page4_table1.png",
+#     model,
+#     r"C:\Users\Elijah\PycharmProjects\edgar_backend\test.png"
+# )
