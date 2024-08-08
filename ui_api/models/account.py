5ee4a130b98737d3300a2aff046862bc8e0540b7
Elijah Anderson <eja11001@DESKTOP-83DSK5F.localdomain>
2024-08-08 06:54:47 -0700

    show api key

--- ui_api/models/account.py
+++ ui_api/models/account.py
@@ -3,6 +3,7 @@ from pydantic import BaseModel
 class AccountInfo(BaseModel):
     username: str
     email: str
+    api_key:str
     cash_flow_route_count: int
     balance_sheet_route_count: int
     income_statement_route_count: int
