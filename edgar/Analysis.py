f3d78cef34fba370e6a9223272bdb567133e3903
elijahanderson96 <elijahanderson96@gmail.com>
2024-12-10 14:43:55 -0800

    refactor: Update FactFrequencyAnalyzer to use new db_connector and schema

--- edgar/Analysis.py
+++ edgar/Analysis.py
@@ -1,9 +1,9 @@
 import pandas as pd
-from database.async_database import AsyncpgConnector
-from config.configs import dsn
+from database.async_database import db_connector
+
 
 class FactFrequencyAnalyzer:
-    def __init__(self, db_connector: AsyncpgConnector):
+    def __init__(self):
         self.db_connector = db_connector
 
     async def analyze_fact_frequency(self) -> pd.DataFrame:
@@ -16,22 +16,21 @@ class FactFrequencyAnalyzer:
         query = """
         WITH total_filed_dates AS (
             SELECT COUNT(DISTINCT filed_date_id) AS total_filed_dates
-            FROM company_facts
+            FROM financials.company_facts
         ),
         total_symbols AS (
             SELECT COUNT(DISTINCT symbol_id) AS total_symbols
-            FROM company_facts
+            FROM financials.company_facts
         )
         SELECT fact_name,
                (COUNT(DISTINCT filed_date_id) * 100.0 / (SELECT total_filed_dates FROM total_filed_dates)) AS filing_percentage,
                (COUNT(DISTINCT symbol_id) * 100.0 / (SELECT total_symbols FROM total_symbols)) AS symbol_percentage
-        FROM company_facts
+        FROM financials.company_facts
         GROUP BY fact_name
         ORDER BY filing_percentage DESC, symbol_percentage DESC;
         """
         return await self.db_connector.run_query(query, return_df=True)
 
-# Example usage
-# db_connector = AsyncpgConnector(dsn=dsn)
-# analyzer = FactFrequencyAnalyzer(db_connector)
-# result = await analyzer.analyze_fact_frequency()
+
+analyzer = FactFrequencyAnalyzer()
+result = analyzer.analyze_fact_frequency()
