cb85099493936268ada220df4b4f80a95a52ee59
elijahanderson96 <elijahanderson96@gmail.com>
2024-12-11 09:11:08 -0800

    feat: Modify query to eliminate duplicate entries for fact names

--- data_models/Views.py
+++ data_models/Views.py
@@ -9,11 +9,29 @@ class MaterializedFinancialsViews:
         WITH latest_filing_dates AS (
             SELECT
                 symbol_id,
+                fact_name,
                 MAX(filed_date_id) AS latest_filed_date_id
             FROM
                 financials.company_facts
             GROUP BY
-                symbol_id
+                symbol_id, fact_name
+        ),
+        latest_facts AS (
+            SELECT
+                f.symbol_id,
+                f.fact_name,
+                MAX(f.start_date_id) AS latest_start_date_id,
+                MAX(f.end_date_id) AS latest_end_date_id,
+                MAX(f.filed_date_id) AS latest_filed_date_id
+            FROM
+                financials.company_facts f
+            JOIN
+                latest_filing_dates lfd
+                ON f.symbol_id = lfd.symbol_id
+                AND f.fact_name = lfd.fact_name
+                AND f.filed_date_id = lfd.latest_filed_date_id
+            GROUP BY
+                f.symbol_id, f.fact_name
         )
         SELECT
             s.symbol,
@@ -23,8 +41,12 @@ class MaterializedFinancialsViews:
         FROM
             financials.company_facts f
         JOIN
-            latest_filing_dates lfd
-            ON f.symbol_id = lfd.symbol_id AND f.filed_date_id = lfd.latest_filed_date_id
+            latest_facts lf
+            ON f.symbol_id = lf.symbol_id
+            AND f.fact_name = lf.fact_name
+            AND f.start_date_id = lf.latest_start_date_id
+            AND f.end_date_id = lf.latest_end_date_id
+            AND f.filed_date_id = lf.latest_filed_date_id
         JOIN
             metadata.symbols s
             ON s.symbol_id = f.symbol_id
