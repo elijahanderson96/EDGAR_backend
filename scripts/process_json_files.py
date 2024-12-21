c60966446a319be5dd70e8f3ca28dec1a743c383
Your Name <you@example.com>
2024-12-21 14:39:16 -0500

    refactor: Remove unnecessary print statements in JSON processing script

--- scripts/process_json_files.py
+++ scripts/process_json_files.py
@@ -14,17 +14,17 @@ directory_path = "companyfacts"
 async def insert_dataframe_to_db(df: pd.DataFrame):
     """Insert a dataframe into the database."""
     # Load symbols and dates tables into memory
-    await db_connector.initialize()
     symbols_df = await db_connector.run_query("SELECT symbol_id, cik FROM metadata.symbols", return_df=True)
     dates_df = await db_connector.run_query("SELECT date_id, date FROM metadata.dates", return_df=True)
-    print(df.columns)
     # Cast columns to appropriate types
     df['cik'] = df['cik'].astype(str)
     df['start'] = pd.to_datetime(df['start'])
     df['end'] = pd.to_datetime(df['end'])
     df['filed'] = pd.to_datetime(df['filed'])
+
     symbols_df['cik'] = symbols_df['cik'].astype(str)
     dates_df['date'] = pd.to_datetime(dates_df['date'])
+
     df_merged_symbols = df.merge(symbols_df, on='cik', how='left')
     df_merged_start_date = df_merged_symbols.merge(dates_df, left_on='start', right_on='date', how='left').rename(
         columns={'date_id': 'start_date_id'})
@@ -32,8 +32,7 @@ async def insert_dataframe_to_db(df: pd.DataFrame):
         columns={'date_id': 'end_date_id'})
     df_merged_filed_date = df_merged_end_date.merge(dates_df, left_on='filed', right_on='date', how='left').rename(
         columns={'date_id': 'filed_date_id'})
-    print(df_merged_filed_date.columns)
-    print(df_merged_filed_date)
+
     # Rename columns to match the database schema
     df = df.rename(columns={
         'fy': 'fiscal_year',
