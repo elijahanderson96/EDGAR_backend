ca30beaf5f9071c0bcbebfc582aa4fa18c0f2ac1
Your Name <you@example.com>
2025-01-04 09:49:53 -0500

    feat: Add decorator to manage database connection for function calls

--- edgar/Analysis.py
+++ edgar/Analysis.py
@@ -1,13 +1,24 @@
 import pandas as pd
 from database.async_database import db_connector
+from functools import wraps
 
 
-class FactFrequencyAnalyzer:
+def manage_db_connection(func):
+    @wraps(func)
+    async def wrapper(self, *args, **kwargs):
+        await self.db_connector.initialize()
+        try:
+            result = await func(self, *args, **kwargs)
+        finally:
+            await self.db_connector.close()
+        return result
+    return wrapper
     def __init__(self):
         self.db_connector = db_connector
 
     fact_names = set()
 
+    @manage_db_connection
     async def fetch_distinct_fact_names(self) -> set:
         """
         Fetch all distinct fact names from the company facts table and set them as a class attribute.
@@ -23,6 +34,7 @@ class FactFrequencyAnalyzer:
         self.fact_names = set(result['fact_name'])
         return self.fact_names
 
+    @manage_db_connection
     async def check_fact_distribution(self, fact_name: str) -> pd.DataFrame:
         """
         Check which symbols have reported a specific fact and which have not, and determine the percentage of filings
@@ -58,6 +70,7 @@ class FactFrequencyAnalyzer:
         """
         return await self.db_connector.run_query(query, params=[fact_name], return_df=True)
 
+    @manage_db_connection
     async def calculate_market_cap(self, symbol: str) -> pd.DataFrame:
         """
         Calculate the market capitalization for a given symbol based on the number of outstanding shares
@@ -102,6 +115,7 @@ class FactFrequencyAnalyzer:
         return await self.db_connector.run_query(query, params=[symbol], return_df=True)
 
 
+    @manage_db_connection
     async def most_common_facts(self, top_n: int = 25) -> pd.DataFrame:
         """
         Identify the most commonly reported fact names within the company facts table.
